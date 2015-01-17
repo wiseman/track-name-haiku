1bbc010ab48e6bc70db82eb105c627494411c11a
wiseman <jjwiseman@gmail.com>
2015-01-16 19:13:15 -0800

    Added Lucene analysis as dependency.

--- project.clj
+++ project.clj
@@ -13,7 +13,8 @@
                  [selmer "0.7.9"]
                  [freebase-clj "0.1.0"]
                  [com.lemonodor/syllables "0.1.0"]
-                 [org.clojure/math.combinatorics "0.0.8"]]
+                 [org.clojure/math.combinatorics "0.0.8"]
+                 [org.apache.lucene/lucene-analyzers-common "4.9.0"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]]
   :hooks [environ.leiningen.hooks]
