77ce8fb1e434d2cb08e1010f903a079ec4334dad
wiseman <jjwiseman@gmail.com>
2015-01-16 19:06:02 -0800

    New test.

---
+++ test/songku/analysis_test.clj
@@ -0,0 +1,23 @@
+(ns songku.analysis-test
+  (:require [clojure.test :refer :all]
+            [songku.analysis :as analysis]))
+
+(deftest analyze-test
+  (testing "tokenize"
+    (let [tokenizer (analysis/make-tokenizer)]
+      (is (= ["hello" "beyonce"]
+             (analysis/tokenize tokenizer "hello Beyoncé")))
+      (is (= ["hello" "5beyonce"]
+             (analysis/tokenize tokenizer "hello 5Beyoncé")))
+      (is (= []
+             (analysis/tokenize tokenizer ""))))
+    (is (= ["hello" "beyonce"]
+           (analysis/tokenize "hello Beyoncé")))
+    (is (= ["hello" "5beyonce"]
+           (analysis/tokenize "hello 5Beyoncé")))
+    (is (= ["hello" "and" "what" "up" "5beyonce"]
+           (analysis/tokenize "hello, & what-up? 5Beyoncé?")))
+    (is (= ["beyonce's"]
+           (analysis/tokenize "Beyoncé's")))
+    (is (= []
+           (analysis/tokenize "")))))
