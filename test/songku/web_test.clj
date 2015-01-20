4722ecaad1370f6d5f9639d890618064b4a32b19
wiseman <jjwiseman@gmail.com>
2015-01-19 21:06:30 -0800

    Added tests.

--- test/songku/web_test.clj
+++ test/songku/web_test.clj
@@ -2,5 +2,9 @@
   (:require [clojure.test :refer :all]
             [songku.web :as web]))
 
-(deftest first-test
-  (is false "Tests should be written"))
+(deftest web-test
+  (testing "parse-log"
+    (is (= 5 (web/parse-long "5"))))
+  (testing "fix-html-entities"
+    (is (= "&" (web/fix-html-entities "&amp;")))
+    (is (= "M&M" (web/fix-html-entities "M&amp;M")))))
